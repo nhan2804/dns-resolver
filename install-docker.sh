2049826b782605f78c9527420803d38c41ef70de
nhan2804 <akainu345@gmail.com>
2024-12-18 18:58:02 +0700

    i

--- install-docker.sh
+++ install-docker.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status
+
 echo "Updating apt..."
+sudo apt-get update -y
 
-sudo apt update
-
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 echo "Adding the GPG key for the official Docker repository..."
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+echo "Adding Docker repository to APT sources..."
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
+sudo apt-get update -y
 
 apt-cache policy docker-ce
 
 echo "Installing Docker CE..."
-
-sudo apt -y install docker-ce
+sudo apt-get install -y docker-ce
 
 # Latest version of docker already comes with compose, so no need to install it
 #echo "Done! Installing docker compose..."
+#sudo apt-get install -y docker-compose
 
-#sudo apt install docker-compose
+echo "Check if Docker is running: sudo systemctl status docker"
 
-echo "Check if docker is running: sudo systemctl status docker"
-
-exit 1
+exit 0
